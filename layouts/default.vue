a9ceb0b3ac8809f6ad433d3c27df2d9a109aa799
Sky <santafejayyt@gmail.com>
2022-08-16 10:17:09 -0700

    change navbar styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,11 @@
         <img src="~/assets/images/sky_full.jpeg" />
       </nuxt-link>
 
-      <div class="navbar-dropdown">
+      <div class="navbar-buttons">
           <ul>
             <li><nuxt-link to="/about">About Me</nuxt-link></li>
-            <li><nuxt-link to="/resume">Resume</nuxt-link></li>
             <li><nuxt-link to="/contact">Contact Form</nuxt-link></li>
+            <li><nuxt-link to="/resume">Resume</nuxt-link></li>
           </ul>
       </div>
     </div>
